879cb60400cf908bbdffd7b226cbf06969916804
raulincze <raulincze@gmail.com>
2015-04-19 20:42:29 +0300

    no message

---
+++ Book/Assets/Scripts/LevelPortal.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelPortal : MonoBehaviour 
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Application.LoadLevel(Application.loadedLevel + 1);
+        }
+    }
+}
